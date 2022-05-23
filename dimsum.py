7e6b531826fea57f11e0ad50fda0c6bb570abd2d
Professional <krosent2@gmail.com>
2022-05-23 21:43:54 +0200

    Continuation on task 2

--- dimsum.py
+++ dimsum.py
@@ -9,35 +9,44 @@ matrix_a = reader.sparse_matrix_a()
 matrix_b = reader.sparse_matrix_b()
 norms = []
 
-# calculating the norms beforehand, this will save time in the iterations. 
-for i in range(1, 31):
+# calculating the norms beforehand, this will save time in the iterations.
+# I assume that we need to take random 4500 columns for each row
+matrix_b.resize(4500, 4500)
+for i in range(4500):
     norms.append(norm(matrix_b.getrow(i)))
 print(norms)
+
 
 def map(row, gamma):
     if row.count_nonzero() > 1:
         ratings = np.delete(row.toarray()[0], np.where(row.toarray()[0] == 0))
-        indices =  np.nonzero(row)[1]
-        combined = [] # making tuples: (index, rating) for each rating (made tuples so the index and rating is easily accessable inside of the iteration below)
+        indices = np.nonzero(row)[1]
+        combined = []  # making tuples: (index, rating) for each rating (made tuples so the index and rating is easily accessable inside of the iteration below)
         for i in range(0, len(ratings)):
             combined.append((indices[i], ratings[i]))
-            
-        pairs = list(combinations(combined, 2)) # creating pairs between every tuple in combined
-        for i in pairs : # iterate over all the pairs
-            first_index = i[0][0]
-            first_rating = i[0][1]
-            second_index = i[1][0]
-            second_rating = i[1][1]
-            
-            probability = min(1, gamma * (1 / (norms[first_index -1] * norms[second_index -1])))
-            print("Probability: ", probability)
 
-            if random.uniform(0,1) <= probability :
-               print("EMIT") #TODO
+        pairs = list(combinations(combined, 2))  # creating pairs between every tuple in combined
+        for ai in pairs:  # iterate over all the pairs
+            first_index = ai[0][0]
+            first_rating = ai[0][1]
+            second_index = ai[1][0]
+            second_rating = ai[1][1]
+
+            aij = norms[first_index - 1]
+            aik = norms[second_index - 1]
+            if aij != 0.0 and aik != 0.0:
+                formula = gamma * (1 / (aij * aik))
+                probability = min(1, gamma * formula)
+                print("Probability: ", probability)
+                rand = random.uniform(0, 1)
+                if rand <= probability:
+                    print("EMIT")  # TODO
 
 def reduce():
     return "reduce"
 
-# Iteration for 10 rows
-for i in range(0,10):
-   map(matrix_a.getrow(i), 0.5)
+
+if __name__ == '__main__':
+    print("shape: " + str(matrix_b.shape))
+    for i in range(0, 10):
+        map(matrix_a.getrow(i), 0.8)
